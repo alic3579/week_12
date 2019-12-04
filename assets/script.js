44e2c9a9fb2db1ce0eee40da8117730fecd5455b
alic3579 <pany468@newschool.edu>
2019-12-04 17:25:33 -0500

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,146 @@
+var counter = "0";
+console.log(counter)
+
+
+var saveUserInfo = function(){
+  if (!storageAvailable('localStorage')){ return; } // if we can use localStorage, don't return
+
+  
+  // localStorage.removeItem('color');
+  // localStorage.removeItem('counter');
+
+
+  // on first site load
+  if(localStorage.getItem('color')){ // using a string
+    // if color has been saved: 
+    var color = localStorage.getItem('color');
+
+    // apply color to element: 
+    document.querySelector("body").style.backgroundColor = color;
+
+  }
+
+
+  if(localStorage.getItem('counter')){ 
+    var counter = parseInt(localStorage.getItem('counter'));
+  }
+
+
+  if(localStorage.getItem('pageCounter')){ 
+    var pageCounter = localStorage.getItem('pageCounter');
+  }
+
+
+
+
+
+  document.getElementById("button").addEventListener('click', function(){
+    // when a user clicks on myDiv, produce a random color to save:
+
+    console.log("clicked!")
+
+    
+    counter++;
+    console.log(counter);
+
+    // var newColor = "rgb(" + red + "," + green + "," + blue + ")";
+    // var newColor = `rgb(${red}, ${green}, ${blue})`;
+    var newColor = `#c98c1a`;
+    var newColor2 = `#e3a228`;
+    var newColor3 = `#e7b14b`;
+    var newColor0 = `#000000`;
+
+
+    if (counter == 1) {
+    localStorage.setItem('color', newColor); // save new color
+     // set new color to item:
+     document.querySelector("body").style.backgroundColor = newColor;
+    } 
+    if (counter == 2) {
+    localStorage.setItem('color', newColor2); // save new color
+     // set new color to item:
+     document.querySelector("body").style.backgroundColor = newColor2;
+    } 
+    if (counter == 3) {
+    localStorage.setItem('color', newColor3); // save new color
+     // set new color to item:
+     document.querySelector("body").style.backgroundColor = newColor3;
+    } 
+    if (counter == 4) {
+      console.log("reset")
+      counter = 0;
+      localStorage.setItem('color', newColor0);
+       // set new color to item:
+     document.querySelector("body").style.backgroundColor = newColor0;
+    }
+
+
+
+    localStorage.setItem('counter', counter);
+
+   
+
+
+  
+
+  })
+
+
+
+/////////////////////////////////////////////
+
+var pageCounter = "0";
+console.log("page" + pageCounter)
+
+
+var home = document.getElementById("home");
+var ch1 = document.getElementById("ch1");
+var ch2 = document.getElementById("ch2");
+var ch3 = document.getElementById("ch3");
+
+document.getElementById("c1").addEventListener('click', function(){
+
+  home.style.display = `none`;
+  ch1.style.display = `block`;
+  ch2.style.display = `none`;
+  ch3.style.display = `none`;
+
+  pageCounter = "1";
+  console.log("page" + pageCounter)
+  localStorage.setItem('pageCounter', pageCounter); 
+
+
+})
+
+document.getElementById("c2").addEventListener('click', function(){
+
+home.style.display = `none`;
+ch2.style.display = `block`;
+ch1.style.display = `none`;
+ch3.style.display = `none`;
+})
+
+document.getElementById("c3").addEventListener('click', function(){
+
+home.style.display = `none`;
+ch3.style.display = `block`;
+ch1.style.display = `none`;
+ch2.style.display = `none`;
+})
+
+document.querySelector("header").addEventListener('click', function(){
+
+home.style.display = `block`;
+ch1.style.display = `none`;
+ch2.style.display = `none`;
+ch3.style.display = `none`;
+
+})
+
+///////////////
+
+
+}
+
+
+saveUserInfo(); // run saving user info
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Week 12</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/main.css">
+
+</head>
+
+
+
+<body>
+
+
+
+<div class="wrapper">
+  <header class="header"> <h1>The General Theory of Employment, Interest and Money</h1><p>John Maynard Keynes (1936)</p></header>
+  <aside class="sideright">   <div id="button"></div> </aside>
+
+  <aside class="sidebar">
+  	<ul>
+  		<li id="c1">Chapter 1</li>
+		<li id="c2">Chapter 2</li>
+		<li id="c3">Chapter 3</li>
+  	</ul>
+  </aside>
+  <article class="content">
+  	<div id="home">
+  		<p>Written: 1935;
+<p>Source: The General Theory of Employment, Interest and Money by John Maynard Keynes, Fellow of the King's College, Cambridge, published by Harcourt, Brace and Company, and printed in the U.S.A. by the Polygraphic Company of America, New York;
+<p>First Published: Macmillan Cambridge University Press, for Royal Economic Society in 1936;
+<p>Transcribed for M.I.A., corrected and formatted by Andy Blunden;
+<p>This Edition: marxists.org 2002.
+  	</div>
+    <div id="ch1" style="display: none;">
+    	<?php include_once("chap1.php"); ?>
+    </div>
+    <div id="ch2" style="display: none;">
+    	<?php include_once("chap2.php"); ?>
+    </div>
+    <div id="ch3" style="display: none;">
+    	<?php include_once("chap3.php"); ?>
+    </div>
+  </article>
+  <footer class="footer">Website by Alicia Pan</footer>
+</div>
+  
+
+	
+
+
+
+	<script type="text/javascript" src="assets/storageAvailable.js"></script>
+	<script type="text/javascript" src="assets/script.js"></script>
+
+</body>
+</html>
